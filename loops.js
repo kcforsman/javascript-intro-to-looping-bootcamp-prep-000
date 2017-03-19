bd62154ff612658d239db4f9720978cb9308517b
Kaitlin Forsman <kcforsman@outlook.com>
2017-03-19 00:21:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+  return "done";
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    array.length = 0;
+  }
+  do {
+    array.shift();
+  } while (maybeTrue());
+  return array;
+}
